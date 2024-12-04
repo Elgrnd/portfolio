e149704c4d7748c0ce720879c8badfd72720af43
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2024-12-04 18:15:26 +0100

    formation

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -19,7 +19,7 @@ export const projects: Project[] = [
         fullDescription: "Ce projet ",
         githubUrl: "https://github.com/Elgrnd/Trains",
         technologies: ["Java", "JavaFX"],
-        imageUrl: "/trains.png",
+        imageUrl: "/assets/img/trains.png",
         features: [
             "Authentification des utilisateurs",
             "Interface responsive",
