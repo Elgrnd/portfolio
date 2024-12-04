f367b8da738763e320b6da9d88acf2cc2bd197c8
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2024-12-04 21:09:41 +0100

    formation

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,5 @@
 import trains from '../../assets/img/trains.png';
+//import nuitinfo from '../../assets/img/nuitinfo.png';
 
 export interface Project {
     id: string;
@@ -18,19 +19,31 @@ export const projects: Project[] = [
         id: "project-1",
         title: "Jeu de société \"Trains\"",
         description: "Projet universitaire en Java/JavaFX",
-        fullDescription: "Ce projet ",
+        fullDescription: "Pour un projet universitaire, j'ai développé une simulation de jeu de gestion ferroviaire" +
+            " intitulée Trains, en utilisant Java pour la logique métier et JavaFX pour l'interface graphique. " +
+            "Ce projet combine des éléments de stratégie, d'optimisation, et de programmation orientée objet pour " +
+            "créer une expérience engageante et interactive.",
         githubUrl: "https://github.com/Elgrnd/Trains",
         technologies: ["Java", "JavaFX"],
         imageUrl: trains,
         features: [
-            "Authentification des utilisateurs",
-            "Interface responsive",
-            "Base de données en temps réel"
+            "Gestion des trains :\n" +
+            "\n" +
+            "Les trains peuvent être ajoutés au réseau, configurés (vitesse, itinéraire), et suivis en temps réel.",
+            "Construction de voies ferrées :\n" +
+            "\n" +
+            "Interface interactive permettant de construire des rails pour relier des villes et des industries.\n" +
+            "Système de coût en ressources pour simuler des contraintes budgétaires.",
+            "Interface utilisateur (UI) avec JavaFX :\n" +
+            "\n" +
+            "Vue dynamique : Représentation graphique des rails, gares, et trains.\n" +
+            "Panneau de contrôle : Gestion centralisée des trains et des itinéraires.",
+
         ],
         challenges: [
-            "Optimisation des performances",
-            "Gestion du state complexe",
-            "Integration de multiples APIs"
+            "Une mécanique de simulation réaliste (gestion des trains, construction de voies ferrées).",
+            "Une interface utilisateur intuitive et visuellement attrayante.",
+            "Une structure de code propre et maintenable, suivant les principes de la programmation orientée objet."
         ],
         outcomes: [
             "Amélioration de l'expérience utilisateur",
